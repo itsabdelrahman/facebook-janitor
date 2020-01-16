a8d2c80dfc79cecd97e0b66d94db75f2bcf75c9c
Abdelrahman Abdelhafez <abdelrahman@maged.rocks>
2020-01-16 21:04:57 +0100

    :recycle: Factor out selectors into objects

--- src/automation/facebook.ts
+++ src/automation/facebook.ts
@@ -15,16 +15,22 @@ export const loginUser = (credentials: Credentials) => async (
 ): Promise<void> => {
   const { email, password } = credentials;
 
+  const selectors = {
+    emailInput: 'input#email',
+    passwordInput: 'input#pass',
+    loginSubmitButton: 'label#loginbutton',
+  };
+
   /* Enter email */
-  await page.focus('input#email');
+  await page.focus(selectors.emailInput);
   await page.keyboard.type(email);
 
   /* Enter password */
-  await page.focus('input#pass');
+  await page.focus(selectors.passwordInput);
   await page.keyboard.type(password);
 
   /* Submit login form */
-  await page.click('label#loginbutton');
+  await page.click(selectors.loginSubmitButton);
   await page.waitForNavigation();
 };
 
@@ -32,8 +38,13 @@ export const loginUser = (credentials: Credentials) => async (
  * Navigates from the Home page to the Profile page
  */
 export const navigateToProfile = async (page: Page): Promise<void> => {
+  const selectors = {
+    profileButton: '#u_0_a > div:nth-child(1) > div:nth-child(1) > div > a',
+  };
+
   /* Click on profile button */
-  await page.click('#u_0_a > div:nth-child(1) > div:nth-child(1) > div > a');
+  await page.waitForSelector(selectors.profileButton);
+  await page.click(selectors.profileButton);
   await page.waitFor(3000);
 };
 
@@ -41,8 +52,13 @@ export const navigateToProfile = async (page: Page): Promise<void> => {
  * Navigates from the Profile page to the Activity Log page
  */
 export const navigateToActivityLog = async (page: Page): Promise<void> => {
+  const selectors = {
+    activityLogButton: 'a._42ft._4jy0._4jy4._517h._51sy',
+  };
+
   /* Click on activity log button */
-  await page.click('a._42ft._4jy0._4jy4._517h._51sy');
+  await page.waitForSelector(selectors.activityLogButton);
+  await page.click(selectors.activityLogButton);
   await page.waitFor(3000);
 };
 
@@ -52,8 +68,15 @@ export const navigateToActivityLog = async (page: Page): Promise<void> => {
 export const selectActivityLogFilter = (filter: ActivityLogFilter) => async (
   page: Page,
 ): Promise<void> => {
+  /* TODO: Activate filter argument */
+
+  const selectors = {
+    filterByPostsButton: '#navItem_statuscluster > a',
+  };
+
   /* Select posts activity log */
-  await page.click('#navItem_statuscluster > a');
+  await page.waitForSelector(selectors.filterByPostsButton);
+  await page.click(selectors.filterByPostsButton);
   await page.waitFor(2000);
 };
 
@@ -61,17 +84,27 @@ export const selectActivityLogFilter = (filter: ActivityLogFilter) => async (
  * Attempts to delete the first post on the Posts Activity Log page
  */
 export const deleteFirstPost = async (page: Page): Promise<void> => {
+  const selectors = {
+    optionsButton: 'a._42ft._42fu._4-s1._2agf._4o_4._p._42gx',
+    deleteOptionButton: "//a[contains(., 'Delete')]",
+    deleteSubmitButton: "//button[contains(., 'Delete')]",
+  };
+
   /* Click on activity log item options */
-  await page.click('a._42ft._42fu._4-s1._2agf._4o_4._p._42gx');
-  await page.waitFor(2000);
+  await page.waitForSelector(selectors.optionsButton);
+  await page.click(selectors.optionsButton);
+  await page.waitFor(2500);
 
   /* Click on activity log item delete option */
-  const [deleteOptionAnchor] = await page.$x("//a[contains(., 'Delete')]");
-  await deleteOptionAnchor.click();
-  await page.waitFor(2000);
+  await page.waitForXPath(selectors.deleteOptionButton);
+  const [deleteOptionButton] = await page.$x(selectors.deleteOptionButton);
+  await deleteOptionButton.click();
+  await page.waitFor(2500);
 
   /* Click on deletion prompt submit button */
-  const [deleteSubmitButton] = await page.$x("//button[contains(., 'Delete')]");
+  await page.waitForXPath(selectors.deleteSubmitButton);
+  const [deleteSubmitButton] = await page.$x(selectors.deleteSubmitButton);
   await deleteSubmitButton.click();
+
   await page.waitFor(5000);
 };
